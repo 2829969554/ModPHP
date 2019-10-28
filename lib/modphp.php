b070e14e0af1da05a0c63c7e738bace61a1ff142
@魔帝本尊 <42338017+2829969554@users.noreply.github.com>
2019-10-28 23:36:11 +0800

    1.9

--- lib/modphp.php
+++ lib/modphp.php
@@ -249,5 +249,27 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 		return false;
 	}
 }
+//判断请求来源是否为电脑，反之为手机
+function isPC(){  
+    $useragent=isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';  
+    $useragent_commentsblock=preg_match('|\(.*?\)|',$useragent,$matches)>0?$matches[0]:'';     
+    function CheckSubstrs($substrs,$text){  
+        foreach($substrs as $substr)  
+            if(false!==strpos($text,$substr)){  
+                return true;  
+            }  
+            return false;  
+    }
+    $mobile_os_list=array('Google Wireless Transcoder','Windows CE','WindowsCE','Symbian','Android','armv6l','armv5','Mobile','CentOS','mowser','AvantGo','Opera Mobi','J2ME/MIDP','Smartphone','Go.Web','Palm','iPAQ');
+    $mobile_token_list=array('Profile/MIDP','Configuration/CLDC-','160×160','176×220','240×240','240×320','320×240','UP.Browser','UP.Link','SymbianOS','PalmOS','PocketPC','SonyEricsson','Nokia','BlackBerry','Vodafone','BenQ','Novarra-Vision','Iris','NetFront','HTC_','Xda_','SAMSUNG-SGH','Wapaka','DoCoMo','iPhone','iPod');  
 
+    $found_mobile=CheckSubstrs($mobile_os_list,$useragent_commentsblock) ||  
+         CheckSubstrs($mobile_token_list,$useragent);  
+
+    if (!$found_mobile){  
+      return true;  
+    }else{  
+      return false;  
+    }  
+}
 ?>
